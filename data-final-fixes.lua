63625d646328ad886163214253389f00f07f973f
Talandar <talandar66@protonmail.com>
2025-12-09 08:46:03 +0100

    crash fix

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -9,14 +9,10 @@ function table.filterKey(t, keyToRemove)
 	return new_table
 end
 
-if settings.startup["generate-oil-only-on-water"].value then
-	data.raw["planet"]["arig"].map_gen_settings.autoplace_controls =
-		table.filterKey(data.raw["planet"]["arig"].map_gen_settings.autoplace_controls, "heavy-oil-geyser")
-	data.raw["planet"]["arig"].map_gen_settings.autoplace_settings.entity.settings = table.filterKey(
-		data.raw["planet"]["arig"].map_gen_settings.autoplace_settings.entity.settings,
-		"heavy-oil-geyser"
-	)
-end
+data.raw["planet"]["arig"].map_gen_settings.autoplace_controls =
+	table.filterKey(data.raw["planet"]["arig"].map_gen_settings.autoplace_controls, "heavy-oil-geyser")
+data.raw["planet"]["arig"].map_gen_settings.autoplace_settings.entity.settings =
+	table.filterKey(data.raw["planet"]["arig"].map_gen_settings.autoplace_settings.entity.settings, "heavy-oil-geyser")
 
 local planet = data.raw.planet and data.raw.planet["arig"]
 local mgs = planet.map_gen_settings or {}
